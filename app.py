81a240fc17d482cd2343b164eb20334188cb0f7f
LutanTech <lutancorpinfoteam@gmail.com>
2026-06-19 20:52:08 +0300

    404 add

--- app.py
+++ app.py
@@ -304,5 +304,9 @@ def search():
     posts = BlogPost.query.filter(BlogPost.title.contains(q)).all()
     return render_template('search.html', posts=posts, q=q)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    print("app.run(debug=True)")
